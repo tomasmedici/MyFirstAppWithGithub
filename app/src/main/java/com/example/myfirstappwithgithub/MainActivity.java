2a805c115c59435b39d2a5377ee83f3341d7b2df
tomasmedici <tomas@medici.dk>
2017-03-09 15:56:49 +0100

    second commit

--- app/src/main/java/com/example/myfirstappwithgithub/MainActivity.java
+++ app/src/main/java/com/example/myfirstappwithgithub/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.myfirstappwithgithub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.MyFirstAppWithGit.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Spinner spinner = (Spinner) findViewById(R.id.spin);
+        String text = spinner.getSelectedItem().toString();
+        intent.putExtra(EXTRA_MESSAGE, text);
+        startActivity(intent);
+    }
+
 }
